037c8720019053b51ece73eadc22a5f41820d0f7
anhnp82 <anhnp82@gmail.com>
2017-04-05 18:37:17 +0200

    LinkedListStack

---
+++ M_Aligned/LinkedListStack.cpp
@@ -0,0 +1,53 @@
+
+struct Node
+{
+    Node(int v)
+    {
+        value = v;
+        next = NULL;
+        prev = NULL;
+    }
+    
+    int value;
+    Node* next;
+    Ndoe* prev;
+}
+
+class Stack
+{
+    public:
+    
+    Stack()
+    {
+        m_Top = NULL;
+    }
+    
+    void push(int v);
+    int pop();
+    
+    protected:
+    
+    Node* m_Top;
+};
+
+void Stack::push(int v)
+{
+    Node* n = new Node(v);
+    n->next = m_Top;
+    m_Top = n;
+}
+
+
+int* Stack::pop()
+{
+    if (m_Top)
+    {
+        int value = m_Top->value;
+        Node n = m_Top;
+        m_Top = m_Top->next;
+        delete n;
+        return value;
+    }
+    else
+        throw exception("empty stack");
+}
